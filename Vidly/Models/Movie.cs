a59fa5ce9be2c0da15af0c6bb01d7c2c0572d1cc
Tony <tony.krikorian@outlook.com>
2019-04-17 22:51:53 -0400

    Validaciones Movie Domain Model
    Se añaden las validaciones a la clase Movie en el Domain Model

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -9,19 +9,25 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="El Nombre es requerido")]
         public string Name { get; set; }
 
         public Genre Genre { get; set; }
 
         [Display(Name = "Genre")]
+        [Required(ErrorMessage = "El Genero es requerido")]
         public int GenreId { get; set; }
 
         [Display(Name = "Release Date")]
+        [Required(ErrorMessage = "El ReleaseDate es requerido")]
         public DateTime? ReleaseDate { get; set; }
 
         public DateTime DateAdded { get; set; }
 
         [Display(Name = "Number In Stock")]
+        [Required(ErrorMessage = "El NumberInStock es requerido")]
+        [Range(1,int.MaxValue,ErrorMessage ="El Stock debe ser mayor a cero")]
         public int NumberInStock { get; set; }
 
     }
