b2efc5bbc1fda820a908db06678474fb8c8a9a78
Tony <tony.krikorian@outlook.com>
2019-04-28 14:36:52 -0400

    Add AutoMapper

---
+++ Vidly/App_Start/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomersDTO>();
+            Mapper.CreateMap<CustomersDTO, Customer>();
+        }
+
+        protected override void Configure()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Vidly/DTO/CustomerDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.DTO
+{
+    public class CustomerDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nombre de usuario requerido")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSuscribedToNewsletter { get; set; }
+
+        [Min18YearsIfAMember(ErrorMessage = "Birth Date is required")]
+        public DateTime? BirthDate { get; set; }
+
+
+
+        public byte MembershipTypeId { get; set; }
+    }
+}
