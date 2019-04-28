2ae954af4d3c84a1599bb060f81b9c7537c2f007
Tony <tony.krikorian@outlook.com>
2019-04-28 14:08:05 -0400

    Create an WebAPI for customers
    Creacion de WebAPI Restful con el CRUD para la entidad de Customers

---
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _dbContex;
+
+        public CustomersController()
+        {
+            _dbContex = new ApplicationDbContext();
+        }
+        // GET/ api/customers
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _dbContex.Customers.ToList();
+        }
+
+        //GET / api/customers/1
+
+        public Customer GetCustomer(int id)
+        {
+            var customer = _dbContex.Customers.SingleOrDefault(x => x.Id == id);
+
+            if (customer == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return customer;
+        }
+
+        // POST /api/customers
+        [HttpPost]
+        public Customer CreateCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            _dbContex.Customers.Add(customer);
+            _dbContex.SaveChanges();
+            return customer;
+        }
+
+        //PUT api/customers/1
+        [HttpPut]
+        public void UpdateCustomer(int id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var customerInDB = _dbContex.Customers.SingleOrDefault(x => x.Id == id);
+
+            if (customerInDB == null)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            customerInDB.Name = customer.Name;
+            customerInDB.BirthDate = customer.BirthDate;
+            customerInDB.IsSuscribedToNewsletter = customer.IsSuscribedToNewsletter;
+            customerInDB.MembershipTypeId = customer.MembershipTypeId;
+
+            _dbContex.SaveChanges();
+        }
+
+        //DELETE api/customers/1
+        [HttpDelete]
+        public void DeleteCustomers(int id)
+        {
+            var customerInDB = _dbContex.Customers.SingleOrDefault(x => x.Id == id);
+
+            if (customerInDB == null)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            _dbContex.Customers.Remove(customerInDB);
+            _dbContex.SaveChanges();
+        }
+    }
+}
