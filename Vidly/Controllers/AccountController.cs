ed79778bd2ef3d29adebd0adb5bfc439c481327a
Tony <tony.krikorian@outlook.com>
2019-05-07 20:35:30 -0400

    Creacion de roles ASP .NET Identity

--- Vidly/Controllers/AccountController.cs
+++ Vidly/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Vidly.Models;
@@ -155,6 +156,18 @@ namespace Vidly.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+
+                    ////Creacion de un role
+                    //var roleStore = new RoleStore<IdentityRole>(new ApplicationDbContext());
+                    //var roleManager = new RoleManager<IdentityRole>(roleStore);
+                    
+                    ////Creamos el role especifico de la aplicacion
+                    //await roleManager.CreateAsync(new IdentityRole("CanManageMovies"));
+
+                    ////Asignamos el usuario a ese role
+                    //await UserManager.AddToRoleAsync(user.Id, "CanManageMovies");
+
+                    //Loguea al usuario
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // Para obtener más información sobre cómo habilitar la confirmación de cuentas y el restablecimiento de contraseña, visite https://go.microsoft.com/fwlink/?LinkID=320771
